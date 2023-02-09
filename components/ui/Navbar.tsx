a7268fc927999b2385edfb624c87f260d74bb881
Jean Carlo Urrego <anarkodev@ANARKOBEATS>
2023-02-09 08:35:05 -0500

    Inicializacion

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import NextLink from 'next/link'
+import { AppBar, Link, Toolbar, Typography, Box, Button, IconButton, Badge } from '@mui/material';
+import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material'
+
+export const Navbar = () => {
+  return (
+    <AppBar>
+        <Toolbar>
+            <NextLink href={'/'} passHref legacyBehavior>
+                <Link display='flex' alignItems='center'>
+                    <Typography variant='h6'>Teslo |</Typography>
+                    <Typography sx={{ ml: 0.5 }}>Shop</Typography>
+                </Link>
+            </NextLink>
+
+            <Box flex={1} />
+
+            <Box sx={{ display: { xs: 'none', sm:'block'} }}>
+                <NextLink href={'/category/men'} passHref legacyBehavior>
+                    <Link>
+                        <Button>Hombres</Button>
+                    </Link>
+                </NextLink>
+
+                <NextLink href={'/category/women'} passHref legacyBehavior>
+                    <Link>
+                        <Button>Mejeres</Button>
+                    </Link>
+                </NextLink>
+
+                <NextLink href={'/category/kids'} passHref legacyBehavior>
+                    <Link>
+                        <Button>Niños</Button>
+                    </Link>
+                </NextLink>
+            </Box>
+
+            <Box flex={1} />
+
+            <IconButton>
+                <SearchOutlined />
+            </IconButton>
+
+            <NextLink href={'/category/cart'} passHref legacyBehavior>
+                <Link>
+                    <IconButton>
+                        <Badge badgeContent={2} color='secondary'>
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </IconButton>
+                </Link>
+            </NextLink>
+
+            <Button>Menu</Button>
+        </Toolbar>
+    </AppBar>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
-export default function Home() {
+import type { NextPage } from 'next';
+import { ShopLayout } from '@Layouts';
+import { Typography } from '@mui/material';
+
+
+const Home:NextPage = () => {
   return (
-    <h1>Hola Mundo</h1>
-  )
-}
+    <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos de Teslo aqui'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb:1 }}>Todos los proudctos</Typography>
+    </ShopLayout>
+  );
+};
+
+export default Home;
