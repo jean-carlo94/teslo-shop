7be2338bd8aef48b4db64ba58ef60c8ae810115d
Jean Carlo Urrego <anarkodev@ANARKOBEATS>
2023-03-17 15:49:53 -0500

    Sin imagenes

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -12,17 +12,22 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
         return res.status(401).json({ message: 'No tiene acceso a este servicio'})
     };
 
-    await db.connect();
+    try {
+        await db.connect();
 
-    await User.deleteMany();
-    await User.insertMany( seedDatabase.initialData.users );
+        await User.deleteMany();
+        await User.insertMany( seedDatabase.initialData.users );
+        await Product.deleteMany();
+        await Product.insertMany( seedDatabase.initialData.products );
+        await Order.deleteMany();
 
-    await Product.deleteMany();
-    await Product.insertMany( seedDatabase.initialData.products );
+        await db.disconnect();
 
-    await Order.deleteMany();
+        res.status(200).json({ message: 'Proceso realizado correctamente' })
 
-    await db.disconnect();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error en Data' }) 
+    };
 
-    res.status(200).json({ message: 'Proceso realizado correctamente' })
-}
+};
